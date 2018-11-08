ffe4e00cd2ac3dd7dbfeab265326984829eef5b7
schwendener <martin.schwendener@epfl.ch>
2018-11-08 22:31:02 +0100

    -_- see above. Same files, muss rausfinden, wie man das andere richtig inkludiert.

--- obstrange.py
+++ obstrange.py
@@ -1,7 +1,19 @@
-from math import ceil, floor
-def obstrange(twr,wt,res):
-    x = twr[0]
-    y = twr[1]
-    range_bl = [floor(i/res) for i in [x-wt/2, y-wt/2, 0]]
-    range_tr = [ceil(i/res) for i in [x+wt/2, y+wt/2, 0]]
-    return range_bl,range_tr
+def obstrange(obst,o_width,res):
+    x = obst[0]/res
+    y = obst[1]/res
+    o_width = o_width/res
+    ox= list()           #
+    oy= list() 
+    # Bottom left corner of the obstacle
+    bl = [int(i) for i in [x-o_width/2, y-o_width/2, 0]]
+
+    # Top right corner of the obstacle 
+    tr = [int(i) for i in [x+o_width/2, y+o_width/2, 0]]
+
+    for i in range(bl[0],tr[0]):
+        for j in range(bl[1],tr[1]):
+            ox.append(i)
+            oy.append(j)
+
+
+    return ox,oy 
