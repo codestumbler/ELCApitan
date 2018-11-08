7810ebed7fc808a5272a6f7a473c68f8afaa9f38
schwendener <martin.schwendener@epfl.ch>
2018-11-08 22:25:34 +0100

    Ensure that random position is not on an obstacle. might fail: last iteration is not checked.

--- get_start_dest.py
+++ get_start_dest.py
@@ -1,13 +1,25 @@
+import numpy as np
 import random 
 
 def get_start_dest(ox,oy): 
     size_x  = max(ox) - min(ox) 
     size_y  = max(oy) - min(oy) 
+
+    # Get initial guess 
     x, y    = ox[0], oy[0]
+    z       = np.array([x,y])
 
-    while (x in ox and y in oy): 
-        x   = random.randint(0,size_x)
-        y   = random.randint(0,size_y) 
-
+    # Assure that position is not on an obstacle
+    obst    = np.array([ox,oy])
+    obstt   = obst.T
+    outside     = False
+    while not outside:  
+        for i in range(len(obstt)):
+            if np.all(z==obstt[:][i]):
+                outside = False
+                x   = random.randint(0,size_x)
+                y   = random.randint(0,size_y) 
+            else:
+                outside = True 
     return x,y
 
