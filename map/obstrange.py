187d1836a8d677030fbec2dace1fe2ad81477f69
= <=>
2018-11-15 20:53:36 +0100

    Obstacles denser, use np.linspace instead of range

--- map/obstrange.py
+++ map/obstrange.py
@@ -1,3 +1,4 @@
+import numpy as np
 def obstrange(obst,o_width,res):
     x = obst[0]/res
     y = obst[1]/res
@@ -5,15 +6,15 @@ def obstrange(obst,o_width,res):
     ox= list()           #
     oy= list() 
     # Bottom left corner of the obstacle
-    bl = [int(i) for i in [x-o_width/2, y-o_width/2, 0]]
+    bl = [float(i) for i in [x-o_width/2, y-o_width/2, 0]]
 
     # Top right corner of the obstacle 
-    tr = [int(i) for i in [x+o_width/2, y+o_width/2, 0]]
+    tr = [float(i) for i in [x+o_width/2, y+o_width/2, 0]]
 
-    for i in range(bl[0],tr[0]):
-        for j in range(bl[1],tr[1]):
-            ox.append(i)
-            oy.append(j)
-
+    # Make obstacle density twice the resolution 
+    for j in np.linspace(bl[0],tr[0], 2/res):
+        for k in np.linspace(bl[1],tr[1], 2/res):
+            ox.append(j)
+            oy.append(k)
 
     return ox,oy 
